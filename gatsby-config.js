a94d934f40b6db04a340eb946ea48b1acb655d0d
Md Shahazad Uddin <shanudjn@gmail.com>
2020-04-25 22:25:31 +0530

    linkedin2

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `md_shanudjn`,
-      linkedin: 'md-shahazad-uddin'
+      linkedin: 'md-shahazad-uddin-2b599614b'
     },
   },
   plugins: [
